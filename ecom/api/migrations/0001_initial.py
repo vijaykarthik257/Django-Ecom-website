097870e60b692b6868cd8d9b632a80ad1bb6b1f3
vijaykarthik257 <80588741+vijaykarthik257@users.noreply.github.com>
2021-03-14 23:03:45 +0530

    Add files via upload

---
+++ ecom/api/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+from api.user.models import CustomUser
+
+
+class Migration(migrations.Migration):
+    def seed_data(apps, schema_editor):
+        user = CustomUser(name="vijay",
+                          email="vijay@lco.dev",
+                          is_staff=True,
+                          is_superuser=True,
+                          phone="8190810262",
+                          gender="Male"
+                          )
+        user.set_password("12345")
+        user.save()
+
+    dependencies = [
+
+    ]
+
+    operations = [
+        migrations.RunPython(seed_data),
+    ]
